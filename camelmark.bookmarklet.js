57bea7de5f62873cc21e9da0b33e56f2c446456a
Jeremy Mack <jeremy@pileofturtles.com>
2011-12-04 12:38:32 -0500

    Bookmarklet now references latest version from github.com

---
+++ camelmark.bookmarklet.js
@@ -0,0 +1,7 @@
+javascript: ((function () {
+    var s = document.createElement('s'+'cr'+'ipt');
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('charset', 'UTF-8');
+    s.setAttribute('src', 'https://raw.github.com/mutewinter/Camelmark/master/camelmark.js');
+    document.documentElement.appendChild(s);
+})());

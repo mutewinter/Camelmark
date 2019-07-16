8808f923199c45ca5533bbf7bb78e5eb57c92da8
David <davidmoore0@gmail.com>
2019-07-15 17:36:10 -0700

    Update camelmark.bookmarklet.js

--- camelmark.bookmarklet.js
+++ camelmark.bookmarklet.js
@@ -2,6 +2,6 @@ javascript: ((function () {
     var s = document.createElement('s'+'cr'+'ipt');
     s.setAttribute('type', 'text/javascript');
     s.setAttribute('charset', 'UTF-8');
-    s.setAttribute('src', 'https://raw.github.com/mutewinter/Camelmark/master/camelmark.js');
+    s.setAttribute('src', 'https://raw.github.com/deltabravozulu/Camelmark/master/camelmark.js');
     document.documentElement.appendChild(s);
 })());

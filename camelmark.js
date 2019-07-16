7eb44d415b17ec7f5a6d3cf268fc00eb6d922c5b
David <davidmoore0@gmail.com>
2019-07-15 17:34:34 -0700

    Update camelmark.js
    Brought back window.open because it's good.

--- camelmark.js
+++ camelmark.js
@@ -1,17 +1,17 @@
 if (document.location.href.match(/amazon/)) {
   if (document.getElementById('ASIN')) {
     var asin = document.getElementById('ASIN').value;
-    document.location = 'http://camelcamelcamel.com/product/' + asin;
+    window.open('http://camelcamelcamel.com/product/' + asin, '_blank');
   }  else if (document.getElementsByName('a')[0]) {
     var asin = document.getElementsByName('a')[0].value;
-    document.location = 'http://camelcamelcamel.com/product/' + asin;
+    window.open('http://camelcamelcamel.com/product/' + asin, '_blank');
   } else {
     alert("No ASIN found on this amazon page, make sure you're on a product page");
   }
 } else if (document.location.href.match(/newegg/)) {
   var productID;
   if ((productID = document.location.href.match(/=(.*)$/)[1])) {
-    document.location = 'http://camelegg.com/product/' + productID;
+    window.open('http://camelegg.com/product/' + productID, '_blank');
   } else {
     alert('No product ID found at end of URL.');
   }
